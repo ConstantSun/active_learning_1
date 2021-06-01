816f1bbdae1654df3bc9fa924c8125ad3681bc7d
Dương Hằng <duonghang0102@gmail.com>
2021-06-02 03:49:03 +0700

    add data dir

--- trainer/setting.py
+++ trainer/setting.py
@@ -6,12 +6,19 @@
 # dir_mask_test = '/data.local/all/hangd/src_code_3/Pytorch-UNet/data_test/masks/'
 
 
-# cotai
-dir_img = "/home/cotai/Phi/active/data/imgs/"
-dir_mask = "/home/cotai/Phi/active/data/masks/"
+# # cotai
+# dir_img = "/home/cotai/Phi/active/data/imgs/"
+# dir_mask = "/home/cotai/Phi/active/data/masks/"
 
-dir_img_test = "/home/cotai/Phi/active/data_test/imgs/"
-dir_mask_test = "/home/cotai/Phi/active/data_test/masks/"
+# dir_img_test = "/home/cotai/Phi/active/data_test/imgs/"
+# dir_mask_test = "/home/cotai/Phi/active/data_test/masks/"
+
+# colab VNU
+dir_img =  "/content/full/data_train/imgs/"
+dir_mask = "/content/full/data_train/masks/"
+
+dir_img_test = "/content/full/data_test/imgs/"
+dir_mask_test = "/content/full/data_test/masks/"
 
 GAUSS_ITERATION = 30
 NUM_FETCHED_IMG = 50
