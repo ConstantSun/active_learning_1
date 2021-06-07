becf42d3f5960c3864045d7fe8a54e2808711913
Dương Hằng <duonghang0102@gmail.com>
2021-06-07 14:16:45 +0700

    add Docker setting

--- trainer/setting.py
+++ trainer/setting.py
@@ -20,5 +20,19 @@ dir_mask = "/content/full/data_train/masks/"
 dir_img_test = "/content/full/data_test/imgs/"
 dir_mask_test = "/content/full/data_test/masks/"
 
+
+# # Docker
+# import sys 
+# import os 
+
+# proj_path = os.path.dirname(os.getcwd())
+# dir_img = f"{proj_path}/full/data/imgs/"
+# dir_mask = f"{proj_path}/full/data/masks/"
+
+# dir_img_test = f'{proj_path}/full/data_test/imgs/'
+# dir_mask_test = f'{proj_path}/full/data_test/masks/'
+
+
+
 GAUSS_ITERATION = 30
 NUM_FETCHED_IMG = 50
